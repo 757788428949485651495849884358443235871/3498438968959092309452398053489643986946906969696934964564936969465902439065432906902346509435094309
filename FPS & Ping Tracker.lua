686259f5b3e1c30c053c929124f246d0b0338be0
757788428949485651495849884358443235871 <123597687+757788428949485651495849884358443235871@users.noreply.github.com>
2023-03-03 18:50:06 -0500

    Create FPS & Ping Tracker.lua

---
+++ FPS & Ping Tracker.lua
@@ -0,0 +1,59 @@
+local PerformanceTracker = Instance.new("TextLabel")
+PerformanceTracker.Name = "FPS Tracker"
+PerformanceTracker.Size = UDim2.new(0, 100, 0, 35)
+PerformanceTracker.BackgroundTransparency = 0.7
+PerformanceTracker.TextColor3 = Color3.new(255, 255, 255)
+PerformanceTracker.TextStrokeTransparency = 1
+PerformanceTracker.TextStrokeColor3 = Color3.new(0.24, 0.24, 0.24)
+PerformanceTracker.Font = Enum.Font.GothamBold
+PerformanceTracker.TextSize = 23
+PerformanceTracker.Text = "FPS: 1"
+PerformanceTracker.BackgroundColor3 = Color3.new(0, 0, 0)
+PerformanceTracker.Position = UDim2.new(1, -135, 0, 1240)
+PerformanceTracker.TextXAlignment = Enum.TextXAlignment.Right
+PerformanceTracker.BorderSizePixel = 3
+PerformanceTracker.Parent = game.CoreGui.RobloxGui
+        spawn(function()
+            local RunService = game:GetService("RunService")
+            local FpsLabel = PerformanceTracker
+            
+            local TimeFunction = RunService:IsRunning() and time or os.clock
+            
+            local LastIteration, Start
+            local FrameUpdateTable = {}
+            
+            local function loopupdate()
+                LastIteration = TimeFunction()
+                for Index = #FrameUpdateTable, 1, -1 do
+                    FrameUpdateTable[Index + 1] = FrameUpdateTable[Index] >= LastIteration - 1 and FrameUpdateTable[Index] or nil
+                end
+            
+                FrameUpdateTable[1] = LastIteration
+                FpsLabel.Text = tostring(math.floor(TimeFunction() - Start >= 1 and #FrameUpdateTable or #FrameUpdateTable / (TimeFunction() - Start))) .. " FPS"
+            end
+            
+            Start = TimeFunction()
+            RunService.Heartbeat:Connect(loopupdate)
+        end)
+    local PerformanceTracker = Instance.new("TextLabel")
+    PerformanceTracker.Name = "Ping Tracker"
+    PerformanceTracker.Size = UDim2.new(0, 100, 0, 35)
+    PerformanceTracker.BackgroundTransparency = 0.7
+    PerformanceTracker.TextColor3 = Color3.new(1, 1, 1)
+    PerformanceTracker.TextStrokeTransparency = 1
+    PerformanceTracker.TextStrokeColor3 = Color3.new(0.24, 0.24, 0.24)
+        PerformanceTracker.Font = Enum.Font.GothamBold
+        PerformanceTracker.TextSize = 23
+        PerformanceTracker.Text = "1 ms"
+        PerformanceTracker.BackgroundColor3 = Color3.new(0, 0, 0)
+        PerformanceTracker.Position = UDim2.new(1, -135, 0, 1280)
+        PerformanceTracker.TextXAlignment = Enum.TextXAlignment.Right
+        PerformanceTracker.BorderSizePixel = 3
+        PerformanceTracker.Parent = game.CoreGui.RobloxGui
+        spawn(function()
+                        game:GetService("RunService").Heartbeat:Connect(function()
+                local ping = tonumber(game:GetService("Stats"):FindFirstChild("PerformanceStats").Ping:GetValue())
+                ping = math.floor(ping)
+                PerformanceTracker.Text = ping.." ms"
+        end)
+end)
